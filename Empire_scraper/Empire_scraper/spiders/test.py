540f1831ef704372dcc83cf5f847f4cc1579f2ef
Unknown <sitara.visram@empire.ca>
2018-05-09 15:44:49 -0400

    Updated test.py to delete items.csv file (contains links from webpage)

--- Empire_scraper/Empire_scraper/spiders/test.py
+++ Empire_scraper/Empire_scraper/spiders/test.py
@@ -1,12 +1,21 @@
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 from Empire_scraper.items import EmpireScraperItem
+import os
 
 
 class MySpider(BaseSpider):
     name = "empire"
     allowed_domains = ["craigslist.org"]
     start_urls = ["http://columbusga.craigslist.org/search/reo/"]
+
+    myfile="/Users/citsbv/dev/empiresrcaper/empirescraper/Empire_scraper/Empire_scraper/items.csv"
+
+    ## if file exists, delete it ##
+    if os.path.isfile(myfile):
+        os.remove(myfile)
+    else:    ## Show an error ##
+        print("Error: %s file not found" % myfile)
 
     def parse(self, response):
         titles = response.selector.xpath("//p")
