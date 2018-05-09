ba2d0660544b5998b8fd567acb3718615c03df43
Unknown <sitara.visram@empire.ca>
2018-05-09 13:46:54 -0400

    Scrapy code using the project name "Empire_scraper"

---
+++ Empire_scraper/Empire_scraper/spiders/test.py
@@ -0,0 +1,19 @@
+from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+from Empire_scraper.items import EmpireScraperItem
+
+
+class MySpider(BaseSpider):
+    name = "empire"
+    allowed_domains = ["craigslist.org"]
+    start_urls = ["http://columbusga.craigslist.org/search/reo/"]
+
+    def parse(self, response):
+        titles = response.selector.xpath("//p")
+        items = []
+        for titles in titles:
+            item = EmpireScraperItem()
+            item["title"] = titles.select("a/text()").extract()
+            item["link"] = titles.select("a/@href").extract()
+            items.append(item)
+        return items
