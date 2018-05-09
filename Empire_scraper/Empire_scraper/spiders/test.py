db8263bf2baf1c78b0fd780be8e5d7313b4101b5
Unknown <millie.tam@empire.ca>
2018-05-09 15:54:27 -0400

    Added comments

--- Empire_scraper/Empire_scraper/spiders/test.py
+++ Empire_scraper/Empire_scraper/spiders/test.py
@@ -11,21 +11,21 @@ class MySpider(BaseSpider):
 
     myfile="/Users/citsbv/dev/empiresrcaper/empirescraper/Empire_scraper/Empire_scraper/items.csv"
 
-    ## if file exists, delete it ##
+    # if file exists, delete it
     if os.path.isfile(myfile):
         os.remove(myfile)
-    else:    ## Show an error ##
+    else:    # show an error
         print("Error: %s file not found" % myfile)
 
     def parse(self, response):
-        titles = response.selector.xpath("//p")
-        items = []
+        titles = response.selector.xpath("//p") 
+        items = []                              
         for titles in titles:
-            item = EmpireScraperItem()
-            item["title"] = titles.select("a/text()").extract()
-            item["link"] = titles.select("a/@href").extract()
+            item = EmpireScraperItem()         
+            item["title"] = titles.select("a/text()").extract()   # gets title
+            item["link"] = titles.select("a/@href").extract()     # gets link
             
-            if ("/6584243056.html" not in str(item["link"])):
+            if ("/6584243056.html" not in str(item["link"])):     # filters out internal sites
                 items.append(item)
     
         return items
