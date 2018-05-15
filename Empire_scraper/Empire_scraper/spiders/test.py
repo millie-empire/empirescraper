77c98f1b79201612b217faafc66adbdae3ef0c95
Unknown <millie.tam@empire.ca>
2018-05-15 10:25:32 -0400

    Scraper recursively scrapes one webpage from start_urls

--- Empire_scraper/Empire_scraper/spiders/test.py
+++ Empire_scraper/Empire_scraper/spiders/test.py
@@ -1,35 +1,38 @@
 import scrapy
-from scrapy.linkextractor import LinkExtractor
-from scrapy.spider import Rule, BaseSpider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import Rule, Spider
 from Empire_scraper.items import EmpireScraperItem
+from scrapy.http import Request
+from scrapy.selector import HtmlXPathSelector
+from pathlib import Path
+
+
 
 
 import os #needed to allow deletion of files
 
 #checks if items.csv file already exists 
-def delete_file():
-        #name of file containing the list of links
-        myfile="/Users/citsbv/dev/empiresrcaper/empirescraper/Empire_scraper/items.csv"
-    
-        if os.path.isfile(myfile): #if file exists, delete it
-            os.remove(myfile)
-    
-        else: #if file not found then show an error     
-            print("Error: %s file not found" % myfile)
+#os.remove("/Users/citmst/dev/empirescraper/Empire_scraper/Empire_scraper/items.csv")
 
-class MySpider(BaseSpider):
+my_file = Path("./items.csv")
+if my_file.is_file():
+   os.remove(my_file)
+
+class MySpider(Spider):
     name = "empire"
-    
-    #calls the method to check is items.csv exists (find where the file is 
-    #created and move it there)
-    delete_file()
 
     #only goes within the internal sites 
     #(finds external sites on the internal site)
-    allowed_domains = ["empire.ca","empirelife.ca","empirelifeinvestments.ca"]
+    #allowed_domains = ["empire.ca","empirelife.ca","empirelifeinvestments.ca"]
+
+    with open('allowed_domains.txt') as file:
+            allowed_domains = []
+            for url in file:
+                allowed_domains.append(url.strip())
+            file.close()
 
     #top-level URL
-    start_urls = ["https://www.empire.ca/", "https://lifeandmoneymatters.empire.ca/", "https://plus.fastandfull.ca/"]
+    #start_urls = ["https://www.empire.ca/", "https://lifeandmoneymatters.empire.ca/", "https://plus.fastandfull.ca/"]
 
     rules = [
         Rule(
@@ -44,27 +47,68 @@ class MySpider(BaseSpider):
 
     # Method which starts the requests by visiting all URLs specified in start_urls
     def start_requests(self):
-        for url in self.start_urls:
-            yield scrapy.Request(url, callback=self.parse, dont_filter=True)
+        with open('start_urls.txt') as file:
+                for url in file:
+                    yield Request(url.strip(), callback=self.parse)
+        file.close()
 
+    items = []  
+    i = 0
+    internal_links = set()
     def parse(self, response):
-        items = []     
-        links = LinkExtractor(canonicalize=True, unique=True).extract_links(response)                         
+          
+             
+        links = LinkExtractor(canonicalize=True, unique=True).extract_links(response)       
+        fields = ["link", "link_from"]                  
+                          
         for link in links:
-            
-            # Check whether the domain of the URL of the link is allowed; so whether it is in one of the allowed domains
+                # Check whether the domain of the URL of the link is external; 
+                # so checks that it is not in allowed_domains
+            #if self.i < 50:
             is_allowed = True
             for allowed_domain in self.allowed_domains:
                 if allowed_domain in link.url:
                     is_allowed = False
-            # If it is allowed, create a new item and add it to the list of found items
+                    # If it is an external link, create a new item and add it to the list of found items
             if is_allowed:
                 item = EmpireScraperItem() 
                 item['link_from'] = response.url
                 item['link'] = link.url
-                items.append(item)
+                
+                if item['link'] not in self.items:
+                    with open('items.csv','a+') as f:
+                        f.write("{}\n".format('\t'.join(str(item[field]) 
+                            for field in fields)))
+                        f.close()
+                    self.items.append(item)
+
+
+            # internal link but not checked and is not a document
+            elif link.url not in self.internal_links and "document" not in link.url:
+                self.internal_links.add(link.url)
+                # recursively checks internal links that have not been checked
+                yield scrapy.Request(link.url, callback=self.parse)
+
+        #self.i += 1
+
+
+        print("hello", self.items)
+        # fields = ["link", "link_from"] # define fields to use
+        # with open('items.csv','a+') as f: # handle the source file
+        #     # f.write("{}\n".format('\t'.join(str(field) 
+        #     #     for field in fields))) # write header
+        #     for url in self.items:
+        #         f.write("{}\n".format('\t'.join(str(url[field]) 
+        #             for field in fields))) # write items
+
+        #return self.items
+
+    # def parse_internal(self, response):
+    #     item = EmpireScraperItem() 
+    #     item['link_from'] = response.url
+    #     item['link'] = response.xpath('a/@href').extract()
         
-        return items
+    #     return item
 
 
 
