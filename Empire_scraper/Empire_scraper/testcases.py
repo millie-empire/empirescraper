1143b99c6345cd2ca94ae994b18aa4227502724c
Unknown <millie.tam@empire.ca>
2018-05-31 14:12:09 -0400

    Reduce amount of calls to read in cells from Google sheets

--- Empire_scraper/Empire_scraper/testcases.py
+++ Empire_scraper/Empire_scraper/testcases.py
@@ -28,27 +28,50 @@ class ScraperTest(unittest.TestCase):
 		for line in output_sheet.col_values(2):
 			self.output_ilink.append(line.strip())
 
-		internal_expected = 1
-		self.expected = {}
-		for line in output_expected.col_values(1):
-			internal_link = output_expected.cell(internal_expected, 2)
-			if line.strip():
-				external_link = line.strip()
-				self.expected[external_link] = [internal_link]
-			else:
-				self.expected[external_link].append(internal_link)
-			internal_expected += 1
 
-		internal_output = 1
-		self.output = {}
-		for line in output_sheet.col_values(1):
-			internal_link = output_sheet.cell(internal_output, 2)
-			if line.strip():
-				external_link = line.strip()
-				self.output[external_link] = [internal_link]
+		self.expected_elink = []
+		self.expected_ilink = []
+		self.expected = {}
+		ext_link = ""
+		i = 0
+		for line in output_expected.col_values(1):
+			self.expected_elink.append(line.strip())
+		for line in output_expected.col_values(2):
+			self.expected_ilink.append(line.strip())
+
+		for link in self.expected_elink:
+			try:
+				int_link = self.expected_ilink[i]
+			except:
+				print("Missing internal link!")
+				int_link = ""
+
+			if link:
+				ext_link = link
+				print("i: ",i, int_link)
+				self.expected[ext_link] = [int_link]
 			else:
-				self.output[external_link].append(internal_link)
-			internal_output += 1
+				self.expected[ext_link].append(int_link)
+			i += 1
+
+		self.output_elink = []
+		self.output_ilink = []
+		self.output = {}
+		ext_link = ""
+		i = 0
+		for line in output_sheet.col_values(1):
+			self.output_elink.append(line.strip())
+		for line in output_sheet.col_values(2):
+			self.output_ilink.append(line.strip())
+
+		for link in self.output_elink:
+			if link:
+				ext_link = link
+				self.output[ext_link] = [self.output_ilink[i]]
+			else:
+				self.output[ext_link].append(self.output_ilink[i])
+			i += 1
+
 
 		#importing allowed domains
 		sheet = client.open("Empire Scraper Input")
@@ -85,6 +108,15 @@ class ScraperTest(unittest.TestCase):
 		for link in self.output_elink:
 			self.assertTrue('.ly' not in link and '.am' not in link and 'redirect' not in link)
 
+	
+	def count_internal(self, links):
+		count = 0
+		for link in links:
+			count += 1
+
+		return count
+
+
 	#check if the scraper is able to get all the external links
 	def test_scrape_all(self):
 
@@ -94,18 +126,13 @@ class ScraperTest(unittest.TestCase):
 			for inter in self.output[ext]:
 				self.assertIn(inter,self.expected[ext])
 			
-			int_count = count_internal(self.output[ext])
-			exp_int_count = count_internal(self.expected[ext])
-			assertTrue(int_count == exp_int_count)
+			int_count = self.count_internal(self.output[ext])
+			exp_int_count = self.count_internal(self.expected[ext])
+			self.assertTrue(int_count == exp_int_count)
 
-		assertTrue(len(self.expected.keys()) == len(self.output.keys()))
+		self.assertTrue(len(self.expected.keys()) == len(self.output.keys()))
 
-	def count_internal(links):
-		count = 0
-		for link in links:
-			count += 1
 
-		return count
 			
 
 #runs all the unit tests
